efbf64a504dbd3874788911746c19474195f0358
tariqul-mulytic <tariqul.hasan@mulytic.io>
2022-05-16 17:52:21 +0600

    updated till part5. All the test fields are added.

--- mytest/polls/tests.py
+++ mytest/polls/tests.py
@@ -1,11 +1,74 @@
+from django.urls import reverse
 from time import time
+from urllib import response
 from django.test import TestCase
 import datetime
 from django.utils import timezone
+from matplotlib.style import context
 
 from .models import Questions
 
+
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Questions.objects.create(question_text=question_text,pub_date=time)
+
 class QuestionsModelTest(TestCase):
+    def test_no_questions(self):
+        """
+        If no questions exist, an appropriate message is displayed.
+        """
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response,'No Polls Found')
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future
+        returns a 404 not found.
+        """
+        future_question = create_question(question_text='Future question.', days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past
+        displays the question's text.
+        """
+        past_question = create_question(question_text='Past Question.', days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
+    def test_future_question_and_past_question(self):
+        """
+        Even if both past and future questions exist, only past questions
+        are displayed.
+        """
+        question = create_question(question_text="Past question.", days=-30)
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            [question],
+        )
+
+    def test_two_past_questions(self):
+        """
+        The questions index page may display multiple questions.
+        """
+        question1 = create_question(question_text="Past question 1.", days=-30)
+        question2 = create_question(question_text="Past question 2.", days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            [question2, question1],
+        )
+
+
     def test_was_published_recently_with_future_question(self):
         """
         was_published_recently() returns False for questions whose pub_date
@@ -16,5 +79,7 @@ class QuestionsModelTest(TestCase):
         future_questions = Questions(pub_date = time)
         self.assertIs(future_questions.was_published_recently(),False)
 
-
-# Create your tests here.
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now() - datetime.timedelta(hours=11,minutes=59,seconds=59)
+        recent_time = Questions(pub_date = time)
+        return self.assertIs(recent_time.was_published_recently(),True)
--- mytest/polls/views.py
+++ mytest/polls/views.py
@@ -29,20 +29,6 @@ class ResultsView(generic.DetailView):
     model = Questions
     template_name = 'polls/results.html'
 
-    # def vote(request, question_id):
-    #     question = get_object_or_404(Questions, pk=question_id)
-    #     try:
-    #         selected_choice = question.choice_set.get(pk = request.POST['choice'])
-    #     except (KeyError, Choice.DoesNotExist):
-    #         return render(request, 'polls/detail.html',{'question':question,'error_message':'Select a correct choice'})
-    #     else:
-    #         selected_choice.vote += 1
-    #         selected_choice.save()
-    #         return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
-    #     #     # Always return an HttpResponseRedirect after successfully dealing
-    #     #     # with POST data. This prevents data from being posted twice if a
-    #     #     # user hits the Back button.
-
 def vote(request, question_id):
     questions = get_object_or_404(Questions, pk=question_id)
     print(questions)
