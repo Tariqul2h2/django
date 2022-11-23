3769d6f69525702d7f13b70a7581858134863434
tariqul-mulytic <tariqul.hasan@mulytic.io>
2022-11-23 19:43:53 +0600

    Tutorial 3 is done, so much painful huh

--- mulytic_site/polls/models.py
+++ mulytic_site/polls/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from datetime import timedelta
 
+
 # Create your models here.
 
 class Questions(models.Model):
@@ -14,8 +15,9 @@ class Questions(models.Model):
     def was_published_recently(self):
         return self.pub_time >= timezone.now() - timedelta(days=1)
 
+
 class Choice(models.Model):
-    question = models.ForeignKey(Questions,on_delete=models.CASCADE)
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
--- mulytic_site/polls/views.py
+++ mulytic_site/polls/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from .models import Questions
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello you are in Poll app')
+    # return HttpResponse('Hello you are in Poll app')
+    latest_question_list = Questions.objects.order_by('-pub_time')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list' : latest_question_list
+    }
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+    return HttpResponse(template.render(context,request))
+
+def detail(request,question_id):
+    # return HttpResponse(f'You are looking at question number {question_id}')
+    try:
+        question = Questions.objects.get(pk=question_id)
+    except Questions.DoesNotExist as e:
+        raise Http404(f'Question {question_id} does not exist')
+    return render(request,'polls/detail.html',{'question':question})
+
+
+def results(request, question_id):
+    return HttpResponse(f'You are looking at the results of question number {question_id}')
+
+def vote(request,question_id):
+    return HttpResponse(f'You are voting on question number {question_id}')
